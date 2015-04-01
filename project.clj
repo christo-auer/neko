dba2230e4d5dfd8ad2e313065ae272b06b8623fe
Alexander Yakushev <alex@bytopia.org>
2015-04-01 15:04:21 +0300

    Update Clojure dependency to alpha6

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/clojure-android/neko"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure-android/clojure "1.7.0-alpha4"]]
+  :dependencies [[org.clojure-android/clojure "1.7.0-alpha6"]]
   :source-paths ["src" "src/clojure"]
   :java-source-paths ["src/java" "gen"]
 
