1fe492c7f190a472d4d2cf1c1cd1114994bb6d94
Alexander Yakushev <alex@bytopia.org>
2015-04-12 20:15:01 +0300

    Prepare 3.2.0 release

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject neko "3.2.0-preview3"
+(defproject neko "3.2.0"
   :description "Neko is a toolkit designed to make Android development using Clojure easier and more fun."
   :url "https://github.com/clojure-android/neko"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure-android/clojure "1.7.0-alpha6"]]
   :source-paths ["src" "src/clojure"]
-  :java-source-paths ["src/java" "gen"]
+  :java-source-paths ["src/java"]
 
   :profiles {:default [:android-common]}
   :plugins [[lein-droid "0.3.0"]]
