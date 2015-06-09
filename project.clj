ab7102aeabe53fcb1dbb224bdf172ce5b7c1557e
Alexander Yakushev <alex@bytopia.org>
2015-06-09 19:49:15 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/clojure-android/neko"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure-android/clojure "1.7.0-alpha6"]]
+  :dependencies [[org.clojure-android/clojure "1.7.0-beta3-r2"]]
   :source-paths ["src" "src/clojure"]
   :java-source-paths ["src/java"]
 
   :profiles {:default [:android-common]}
-  :plugins [[lein-droid "0.3.0"]]
+  :plugins [[lein-droid "0.4.0-alpha2"]]
 
   :android {:library true
             :target-version 18})
